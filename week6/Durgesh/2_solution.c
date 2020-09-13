90aec34dfde15faf64709e44c86d518f5f218662
Durgesh Agrawal <durgeshagrawal1911@gmail.com>
2020-09-13 12:31:19 +0530

    Add week6 1 question

---
+++ week6/Durgesh/2_solution.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void reverseSwap(int* A, int* B, int n){
+    for(int i=0; i<n; i++){
+        int temp = A[i];
+        A[i] = B[n-i-1];
+        B[n-i-1] = temp;
+    }
+    return;
+}
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    int A[n];
+    int B[n];
+    
+    for (int i=0; i<n; i++) scanf("%d", &A[i]);   
+    for (int i=0; i<n; i++) scanf("%d", &B[i]);
+
+    reverseSwap(A, B, n);   
+
+    for(int i=0; i<n; i++) printf("%d ", A[i]);
+    printf("\n");
+    for(int i=0; i<n; i++) printf("%d ", B[i]);
+
+    return 0;
+}
